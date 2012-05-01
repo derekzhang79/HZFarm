51f40ef2bcd4977608dce46d2add814c495f19a4
liwensheng <liwensheng@localhost.(none)>
2012-05-01 09:40:28 +0800

    Initial Commit

---
+++ HZFarm/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  HZFarm
+//
+//  Created by liwensheng on 12-5-1.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ HZFarm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HZFarm
+//
+//  Created by liwensheng on 12-5-1.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
